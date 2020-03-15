5d4d8abc3ef50866874b5e9519a0da6e3735e6fa
tmanderson001 <tmanderson001@gmail.com>
2020-03-15 04:14:24 -0400

    adding work1

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+var randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbmol,
+};
+
+
+
+generatePassword(getRandomNumber, getRandomSymbmol, getRandomUpper, getRandomLower);
+
+  // 
+
+  function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+    
+    // Doesn't have a selected type
+    if(typesCount === 0) {
+      return '';
+    }
+    
+    // create a loop
+    for(let i=0; i<length; i+=typesCount) {
+      typesArr.forEach(type => {
+        const funcName = Object.keys(type)[0];
+        generatedPassword += randomFunc[funcName]();
+      });
+    }
+    
+    const finalPassword = generatedPassword.slice(0, length);
+    
+    return finalPassword;
+  }
+
+console.log(lower, upper, number);
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+console.log(randomFunc());
+
 // Generator functions - http://www.net-comber.com/charset.html
 
 function getRandomLower() {
@@ -19,31 +74,5 @@ function getRandomSymbmol() {
  var symbols = '!@#$%^&*()[],./{}|:<>?';
  return symbols[Math.floor(Math.round() * symbols.length)];
 }
-
-var randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbmol,
-};
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-function generatePassword () {
-
-}
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 

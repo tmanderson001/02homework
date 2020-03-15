cae5aa1a403f85dd689eac60e0e96f9ab7d38167
tmanderson001 <tmanderson001@gmail.com>
2020-03-15 01:01:40 -0400

    adding work1

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Generator functions - http://www.net-comber.com/charset.html
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) +97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.round() * 26) + 65);
+}
+
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbmol() {
+ var symbols = '!@#$%^&*()[],./{}|:<>?';
+ return symbols[Math.floor(Math.round() * symbols.length)];
+}
+
+console.log(getRandomSymbmol());
